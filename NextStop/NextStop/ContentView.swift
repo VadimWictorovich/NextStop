51b8b1949604285967237da9f7f1548f0131e644
Vadim <91436203+VadimWictorovich@users.noreply.github.com>
2025-02-21 17:03:33 +0300

    project created

---
+++ NextStop/NextStop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NextStop
+//
+//  Created by Вадим Игнатенко on 21.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NextStop/NextStop/NextStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextStopApp.swift
+//  NextStop
+//
+//  Created by Вадим Игнатенко on 21.02.25.
+//
+
+import SwiftUI
+
+@main
+struct NextStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
